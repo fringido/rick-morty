124864711628bea205a9c0af20faded1cebea3f0
Fringido <mmaaxx77@msn.com>
2021-08-23 19:57:53 -0500

    terinado

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,9 @@
+const resolveRouters = (route) => {
+    if (route.length <= 3) {
+        let validRoute = route === '/' ? route : '/:id';
+        return validRoute;
+    }
+    return `/${route}`;
+};
+
+export default resolveRouters;
